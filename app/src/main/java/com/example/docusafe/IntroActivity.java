3c0033aaad22bbaa242d7ac9600d13d29652b669
LinhNguyenTien <linhtiennguyenz@gmail.com>
2023-09-19 19:53:18 +0700

    Docusafe ver 1.2

--- app/src/main/java/com/example/docusafe/IntroActivity.java
+++ app/src/main/java/com/example/docusafe/IntroActivity.java
@@ -2,13 +2,33 @@ package com.example.docusafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IntroActivity extends AppCompatActivity {
-
+    private Button signInBtn;
+    private Button registerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        signInBtn = findViewById(R.id.signInBtn);
+        registerBtn = findViewById(R.id.registerBtn);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/docusafe/LoginActivity.java
+++ app/src/main/java/com/example/docusafe/LoginActivity.java
@@ -3,12 +3,38 @@ package com.example.docusafe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView usernameTV;
+    private TextView passwordTV;
+    private TextView forgotTV;
+    private Button signInBtn;
+    private TextView registerTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Connect to views
+        usernameTV = findViewById(R.id.usernameInput);
+        passwordTV = findViewById(R.id.passwordInput);
+        forgotTV = findViewById(R.id.forgetPasswordBtn);
+        signInBtn = findViewById(R.id.signInBtn);
+        registerTV = findViewById(R.id.registerLinking);
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if username and password is correct
+                //then login the main interface
+                //else create an error toast
+            }
+        });
     }
 }
